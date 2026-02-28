b01468e9fdf86777d2ee77d1cfb833eb5e7fcc9a
Brian Brick <brickforbrains@gmail.com>
2026-02-28 15:38:22 -0800

    feat: add ILogicComponent interface to Core library

---
+++ src/Core/ILogicComponent.cs
@@ -0,0 +1,13 @@
+﻿namespace OmniLink.Core.Interfaces;
+
+/// <summary>
+/// The fundamental contract for anything the processes logic in the game.
+/// </summary>
+internal interface ILogicComponent
+{
+    string ComponentId { get;  }
+    bool isActive { get; set; }
+
+    //Taking an input state and returning the resulting output.
+    bool Evaluate(bool inputState);
+}
